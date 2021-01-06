abb01398ae692c0e8a78e83f9c3f5708f4afd038
lauraharston <lauraharston@gmail.com>
2021-01-05 18:14:39 -0600

    Created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button
+} from 'reactstrap';
+
+import logo from "../images/loflix-logo.png";
+
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar dark expand="md">
+        <NavbarBrand href="/">
+          <img src={logo} height="90" width="90" alt="loflix-logo" />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Movies</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">TV Shows</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
